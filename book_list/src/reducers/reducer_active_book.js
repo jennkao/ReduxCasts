68a725e9d81be569cb5250e888d1b1d2a342d12c
Jennifer Kao <jennkao92@gmail.com>
2017-03-17 22:35:40 -0700

    add book detail container

--- book_list/src/reducers/reducer_active_book.js
+++ book_list/src/reducers/reducer_active_book.js
@@ -1,10 +1,26 @@
+// export default function(state = null, action) {
+//   switch(action.type) {
+//   case 'BOOK_SELECTED':
+//     return action.payload;
+//   }
+
+//   return state;
+// }
+
 // State argument is not application state, only the state
 // this reducer is responsible for
+//redux won't let you return undefined value;
 export default function(state = null, action) {
   switch(action.type) {
-  case 'BOOK_SELECTED':
-    return action.payload;
+    case 'BOOK_SELECTED' : 
+    //don't mutate state, always return a fresh object--> this is new state
+      return action.payload;
+
   }
 
   return state;
 }
+
+//redux sends some booting up signals through the reducers
+//action comes through, don't know what the type is so we
+//just return state (and default state is null)
